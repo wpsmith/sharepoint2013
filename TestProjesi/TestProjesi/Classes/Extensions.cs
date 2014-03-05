628ce9b4019ddcf52ec1ca0bf0a76d80b32b5ace
kaanuzer <kaanuzer@gmail.com>
2014-03-05 11:35:23 +0200

    Classların eklenmesi

---
+++ TestProjesi/TestProjesi/Classes/Extensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace TestProjesi.Classes
+{
+    static class Extensions
+    {
+
+        /// <summary>
+        /// if this list is on the current web return false else return true
+        /// </summary>
+        /// <param name="spWeb"></param>
+        /// <param name="listName"></param>
+        /// <returns></returns>
+        public static bool IsThereAList(this SPWeb spWeb, string listName)
+        {
+            SPList list = spWeb.Lists.TryGetList(listName);
+            return list == null ? true : false;
+        }
+        /// <summary>
+        /// Return field Static Name 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="staticFieldName"></param>
+        /// <returns></returns>
+        public static string TryGetListFieldInternalName(this SPList list, string staticFieldName)
+        {
+            return list.Fields.TryGetFieldByStaticName(staticFieldName).InternalName;
+        }
+        /// <summary>
+        /// Change the Title Display Name 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="TitleName"></param>
+        public static void ChangeTitleDisplayName(this SPList list, string TitleName) 
+        {
+            if (list!=null)
+            {
+                SPField titlefield = list.Fields["Title"];
+                titlefield.Title = TitleName;
+                titlefield.PushChangesToLists = true;
+                titlefield.Update(true);
+            }
+            
+        }
+    }
+}
+
---
+++ TestProjesi/TestProjesi/Classes/SaveLogs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+
+namespace TestProjesi.Classes
+{
+    class SaveLogs
+    {
+        public static void newLogItem(Exception ex, string LogLocation)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate()
+            {
+                SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory(LogLocation, TraceSeverity.Unexpected, EventSeverity.Error), TraceSeverity.Unexpected, ex.Message, ex.StackTrace);
+            });
+        }
+    }
+}
